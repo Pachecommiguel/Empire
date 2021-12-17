ac0f8ceb230fbb64c51420b9086ea10d0743c3c6
Miguel Matos <pacheco.mmiguel@gmail.com>
2021-12-17 15:52:34 +0000

    Add CharacterRepository.kt

---
+++ app/src/main/java/com/example/empire/persistence/CharacterRepository.kt
@@ -0,0 +1,10 @@
+package com.example.empire.persistence
+
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class CharacterRepository @Inject constructor(
+
+) {
+}
--- app/src/main/java/com/example/empire/ui/viewmodels/CharactersFragmentViewModel.kt
+++ app/src/main/java/com/example/empire/ui/viewmodels/CharactersFragmentViewModel.kt
@@ -2,13 +2,14 @@ package com.example.empire.ui.viewmodels
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.empire.persistence.CharacterRepository
 import com.example.empire.persistence.entities.Character
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class CharactersFragmentViewModel @Inject constructor(
-
+    private val repository: CharacterRepository
 ) : ViewModel() {
 
     val characterList = MutableLiveData<List<Character>>()
